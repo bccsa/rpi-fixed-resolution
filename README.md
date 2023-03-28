92f06c05ab204a53d02e7df5117283f654e480ae
Ivan Slabbert <ivan@slabbert.org>
2023-03-28 11:41:05 +0200

    set hdmi sound

--- README.md
+++ README.md
@@ -1,21 +1,11 @@
 # rpi-fixed-resolution
-SystemD service to force a fixed screen resolution for Raspberry Pi OS Bullseye.
+Process to force a fixed screen resolution for Raspberry Pi OS Bullseye.
 
-rpi-fixed-resolution forces the display resolution, refresh rate and position to the values configured in the configuration file.
+rpi-fixed-resolution forces the display resolution, refresh rate and position to the values configured in the configuration file. The sound output is automatically set to the first connected HDMI display.
 
 ## Prerequisites
 * nodejs
 * xrandr
 
-## Installation & updates
-Installing or updating the SystemD service:
-```shell
-sudo ./install.sh /path/to/configuration/file.json
-```
-Uninstalling the SystemD service:
-```shell
-sudo ./uninstall.sh
-```
-
 ## Configuration
-An default configuration file (/etc/rpi-fixed-resolution) is automatically generated on first run. Note that the default configuration will probably not work as a valid username needs to be configured under which the xrandr command will run (logged in user).
+The configuration file path is passed to the NodeJS script as a parameter. If no parameter is passed the default path is used. If no configuration file exists, the script will attempt to write its default configuration to the passed / default configuration file.
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const fs = require('fs');
+const { EventEmitter } = require('events');
+
+const event = new EventEmitter();
 
 // Default config
 var conf = {
@@ -76,21 +79,75 @@ function saveConfig(path, data) {
     });
 }
 
-// xrandr screen configuration
-function xrandr(conf) {
-    let cmd = `sudo -u ${conf.user} DISPLAY=:0 xrandr --output HDMI-1 --mode ${conf.resolution} --rate ${conf.refreshRate} --pos ${conf.position};
-               sudo -u ${conf.user} DISPLAY=:0 xrandr --output HDMI-2 --mode ${conf.resolution} --rate ${conf.refreshRate} --pos ${conf.position};`;
-
+// monitors list of connected displays, and fires an 'connected' event when a display is connected
+var xrandrState = {};
+// var hdmiSoundEnabled = false;
+function xrandrMonitor(conf) {
+    let cmd = `DISPLAY=:0 xrandr | grep HDMI`
     exec(cmd, { shell: true }).then(output => {
+        if (typeof output.stdout == 'string') {
+            output.stdout.split('\n').forEach(line => {
+                let display = line.match(/HDMI-[1-2]/gmi);
+                let state = line.match(/(dis)?connected/gmi);
 
-    }).catch(err => {});
+                if (Array.isArray(display) && display.length > 0) {
+                    display = display[0];
+                }
+
+                if (Array.isArray(state) && state.length > 0) {
+                    state = state[0];
+                }
+
+                if (display && state) {
+                    // If an HDMI display is connected on first run, set hdmiSoundEnabled to true to prevent running the sound configuration twice
+                    // if (state == 'connected' && !xrandrState[display]) {
+                    //     hdmiSoundEnabled = true;
+                    // }
+
+                    // Fire event if state changes to connected
+                    if (state == 'connected' && state != xrandrState[display]) {
+                        event.emit('connect', display);
+                    }
+
+                    xrandrState[display] = state;
+                }
+            });
+        }
+    }).catch(err => { });
 }
+
+// Listen for the monitor 'connect' event
+event.on('connect', display => {
+    // Set resolution and refresh rate
+    let cmd1 = `DISPLAY=:0 xrandr --output ${display} --mode ${conf.resolution} --rate ${conf.refreshRate} --pos ${conf.position}`;
+    exec(cmd1, { shell: true }).catch(err => { });
+
+    // Set sound output
+    // ref1 https://forums.raspberrypi.com/viewtopic.php?t=343523
+    // ref2 https://unix.stackexchange.com/questions/65246/change-pulseaudio-input-output-from-shell
+    let cmd2 = `
+        DISPLAY=:0 systemctl --user stop pulseaudio.service
+        DISPLAY=:0 systemctl --user stop pulseaudio.socket
+        DISPLAY=:0 pulseaudio --start
+        DISPLAY=:0 pacmd set-default-sink 1`;
+    // if (!hdmiSoundEnabled) {
+    //     cmd2 = `
+    //         systemctl --user stop pulseaudio.service
+    //         systemctl --user stop pulseaudio.socket
+    //         pulseaudio --start
+    //         pacmd set-default-sink 1`;
+    //     hdmiSoundEnabled = true;
+    // } else {
+    //     cmd2 = `pacmd set-default-sink 1`;
+    // }
+    exec(cmd2, { shell: true }).catch(err => { });
+});
 
 // Start interval timer
 function startCycle(conf) {
     // first run
-    xrandr(conf);
+    xrandrMonitor(conf);
 
     // Subsequent runs
-    setInterval(() => { xrandr(conf) }, conf.interval);
+    setInterval(() => { xrandrMonitor(conf) }, conf.interval);
 }
